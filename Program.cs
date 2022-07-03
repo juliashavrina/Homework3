f7564c5ee48d9d51fb50d75ac98797f1a834c3a2
«juliashavrina» <tifanys@yandex.ru>
2022-07-03 17:45:31 +0300

    Homework3

--- Program.cs
+++ Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿/*void PalNumVoid()
+{
+    int num;
+    Console.WriteLine("Введите число от 10000 до 99999 " );
+    num = Convert.ToInt32(Console.ReadLine());
+
+    int FirstNum = (num / 10000) % 10;
+    int SecondNum = (num / 1000) % 10;
+    int ForthNum = (num / 10) % 10;
+    int FifthNum = num % 10;
+
+    if (FirstNum == FifthNum && SecondNum == ForthNum) 
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+        
+}
+PalNumVoid();
+*/
+
+/*void CubeNumVoid()
+{   
+    int N;
+    Console.Write("Input a number: ");
+    N = Convert.ToInt32(Console.ReadLine());
+    int current = 1;
+    while (current <= N)
+    {
+       int cube = current * current * current;
+        Console.Write(cube + " ");
+        current = current + 1;
+
+    }
+}
+
+CubeNumVoid();
+*/
